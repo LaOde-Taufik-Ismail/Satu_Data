51ac649d055552d14ab0698e8962daea9c53ea41
LaOde-Taufik-Ismail <laodetaufikismail@gmail.com>
2026-02-20 11:01:47 +0800

    Menambahkan konfigurasi deploy gh-pages dan vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 // IMPORT KOMPONEN
 import Navbar from "./components/Navbar";
@@ -13,22 +13,20 @@ import Infografis from "./pages/Infografis";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-slate-50 font-sans">
-        <Navbar />
+    <div className="flex flex-col min-h-screen bg-slate-50 font-sans">
+      <Navbar />
 
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/infografis" element={<Infografis />} />
-            <Route path="/produsen/:kategori" element={<ListInstansi />} />
-            <Route path="/instansi/:instansiId" element={<KatalogData />} />
-          </Routes>
-        </div>
-
-        <Footer />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/infografis" element={<Infografis />} />
+          <Route path="/produsen/:kategori" element={<ListInstansi />} />
+          <Route path="/instansi/:instansiId" element={<KatalogData />} />
+        </Routes>
       </div>
-    </Router>
+
+      <Footer />
+    </div>
   );
 }
 
